36b57c00363cb86f5f7df5bdeffb9cc492443802
leritwo <leticia.trujillo@g.austincc.edu>
2021-02-05 21:21:02 -0600

    Homework1_multiply_numbers_LT

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -11,12 +11,11 @@ int main()
 	int result_variable;//create result variable
 	cout<< "Please insert a number:";//message to user to insert number
 	cin>> num;//convert the number inputted to the variable num
-	//multiply_numbers( num);//use num as the parameter in the function
-	result_variable=multiply_numbers(num);
+	result_variable=multiply_numbers(num);//use num as the parameter in the function
 	cout<< num<<" multiplied by 5 "<<"equals\n";//display the multiplication of the num&num2
 	cout<<result_variable<<"\n";//dislay result of the multiplication
 	int num1=4;//create int variable equal to 4
-	result_variable=multiply_numbers(num1);
+	result_variable=multiply_numbers(num1);//use num1 as the parameter in the function
 	cout<< num1<<" multiplied by 5 "<<"equals\n";//display the multiplication of the num1 & num2
 	cout<<result_variable;//dislay result of the multiplication
 	return 0;
--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -11,6 +11,7 @@ int multiply_numbers(int num1)
 {
 	int num2=5;
 	return num1*num2;
+
 }
 
 
