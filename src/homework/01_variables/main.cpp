7fd290ffa19401bdc6fbc3f60d79f16407b1fa48
leritwo <leticia.trujillo@g.austincc.edu>
2021-02-12 20:40:30 -0600

    HW1(2/12/21)

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,31 @@
 ﻿//write include statements
-
+#include "variables.h"
 
 //write namespace using statement for cout
-
+#include <iostream>
+using namespace std;
+//write header file to use setprecision
+#include <iomanip>
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
-
+	double meal_amount;//create variable for meal cost
+	double tip_rate;//create variable for wanted tip rate
+	double tax_amount;//create variable for tax cost
+	double total;//create variable for total cost
+	cout<<"Enter the meal cost:";//get user to input meal cost
+	cin>>meal_amount;//assign the input to the meal cost variable
+	tax_amount=get_sales_tax_amount(meal_amount);//assign the return value from function to variable
+	cout<<"Enter desired tip percentage, in percent form:";//get user to input desired tip rate
+	cin>>tip_rate;//assign imput to the tip rate variable
+	tip_rate=get_tip_amount(meal_amount,tip_rate);//assign the return value from function to variable
+	total=tip_rate + tax_amount + meal_amount;//get restaurant total
+	cout<<"Meal Amount:$"<<meal_amount<<"\n";//display meal total
+	cout<<"Sales Tax:$"<<fixed<<showpoint<<setprecision(2)<<tax_amount<<"\n";//display tax total
+	cout<<"Tip Amount:$"<<fixed<<showpoint<<setprecision(2)<<tip_rate<<"\n";//display tip total
+	cout<<"Total:$"<<fixed<<showpoint<<setprecision(2)<<total<<"\n";//display restaurant total
 	return 0;
 }
--- src/homework/01_variables/variables.h
+++ src/homework/01_variables/variables.h
@@ -2,6 +2,11 @@
 int add_numbers(int num1, int num2);
 
 //write function prototype for multiply_numbers with two integer parameters num1, and num2
+//create a double return function prototype 
+double get_sales_tax_amount(double meal_amount);
+
+//create a double value return prototype
+double get_tip_amount(double meal_amount, double tip_rate);
 
 
 
